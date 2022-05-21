3159d4fd7ae6aa404c82eacea55c50cf75062888
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-05-21 16:33:21 +0500

    Basic arthimetic operations code

--- 1.Basic_Hello_World_Program/HelloWorld.dart
+++ 1.Basic_Hello_World_Program/HelloWorld.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 void main() {
   print("Hello World");
 }
---
+++ Arthimetic_Operations.dart
@@ -0,0 +1,14 @@
+void main() {
+/**
+ Add: +
+Subtract: -
+Multiply: *
+Divide: /
+ 
+ */
+
+  print(1 + 2);
+  print(4 - 2);
+  print(1 / 2);
+  print(2 * 2);
+}
