13e3f0c82365b5e697194bc7f77465eaed93d0dc
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-05-26 16:57:29 +0500

    basic dart function

--- 4.Functions/Functions.dart
+++ 4.Functions/Functions.dart
@@ -1,1 +1,44 @@
-void main() {}
+void main() {
+  String functionName(int number) {
+    return "Hello";
+  }
+
+ 
+  }
+  
+  
+
+  // calling a function
+
+  /**
+  * Return type: This comes first; it tells you immediately what
+the type will be of the function output. This particular
+function will return a String, but your functions can return
+any type you like. If the function won’t return anything,
+that is, if it performs some work but doesn’t produce an
+output value, you can use void as return type.
+
+
+Function name: You can name functions almost anything
+you like, but you should follow the lowerCamelCase naming
+convention.
+
+Parameters: Parameters are the input to the function; they
+go inside the parentheses after the function name. This
+example has only one parameter, but if you had more than
+one, you would separate them with commas. For each
+parameter, you write the type first, followed by the name.
+Just as with variable names, you should use lowerCamelCase
+for your parameter names.
+
+Return value: This is the function’s output, and it should
+match the return type. In the example above, the function
+returns a String value by using the return keyword. If the
+return type is void, though, then you don’t return anything.
+
+
+The return type, function name and parameters are collectively
+known as the function signature. The code between the braces
+is known as the function body.
+ */
+}
