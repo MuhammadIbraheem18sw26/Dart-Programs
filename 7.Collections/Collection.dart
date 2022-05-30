a3f2a74c881c3f200b17590cc641235fa4843ec6
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-05-30 18:56:00 +0500

    added remove and add methods of list and using unmodofiable method

--- 7.Collections/Collection.dart
+++ 7.Collections/Collection.dart
@@ -2,10 +2,29 @@ void main() {
   // List
   List<String> snacks = [];
   //    OR
-  var dessert = <String>["appple", "mango"];
-  int count = 0;
-  for (var element in dessert) {
-    print(element + "index" + element[count]);
-    count++;
+  var dessert = <String>["apple", "mango"];
+  print(dessert);
+
+  // iterate list with forIn loop
+
+  void iterate({List<String>? list}) {
+    for (var element in dessert) {
+      print(element);
+    }
   }
+
+// add element
+  dessert.add("orange");
+  iterate(list: dessert);
+
+//remove element
+  dessert.remove('apple');
+  iterate(list: dessert);
+
+  // unmodifiable lsit
+  final dessert2 = List.unmodifiable(dessert);
+  print(dessert2);
+
+  // dessert2.add("hello"); not allowed --> runtime error
+  print(dessert);
 }
