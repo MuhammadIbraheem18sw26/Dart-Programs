8f82e6c67d48d2b267540fde399923945fbe61a9
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-06-04 20:39:04 +0500

    simple example of inheritance

---
+++ Classes.dart
@@ -0,0 +1,26 @@
+enum Grade { A, B, C, D }
+
+class Person {
+  String givenName;
+  String surName;
+
+  Person(this.givenName, this.surName);
+
+  String get fullName => '$givenName $surName';
+
+  @override
+  String toString() => fullName;
+}
+
+class Student extends Person {
+  Student(String givenName, String surName) : super(givenName, surName);
+  var grades = <Grade>[];
+}
+
+void main() {
+  final asim = Student("Asim", "Malik");
+  final ibrahim = Person("Ibraheem", "Jhullan");
+
+  print(asim.fullName);
+  print(ibrahim.fullName);
+}
