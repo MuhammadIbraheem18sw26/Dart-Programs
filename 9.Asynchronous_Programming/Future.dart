e468ab64d4ce6622f2dfc0d7d58117ef0f79b6a5
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2022-06-08 22:07:36 +0500

    instace of Future<int?> with callback function

---
+++ 9.Asynchronous_Programming/Future.dart
@@ -0,0 +1,16 @@
+void main() {
+  print('before the future');
+  final delay = Future<int?>.delayed(Duration(seconds: 1), () => 42)
+      .then(
+        (value) => print('Value: $value'),
+      )
+      .catchError(
+        (error) => print('Error: $error'),
+      )
+      .whenComplete(
+        () => print('Future is complete'),
+      );
+  print('After the future');
+
+  print("after the future");
+}
